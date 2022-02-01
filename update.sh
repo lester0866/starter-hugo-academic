b6e376a564a7eb7babc4f840b74e61869df89d0d
lester0866 <lester0866@gmail.com>
2022-01-31 19:51:30 -0500

    update

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git add .
 git commit -m "${1}"
 git push -u origin master
 
-hugo # Generate the website inside the public directory
+hugo.exe # Generate the website inside the public directory
 cd public
 git add .
 git commit -m "Build website"
