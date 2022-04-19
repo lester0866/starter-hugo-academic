ba7b90e96b39939b06e7542feb6c013820d9db21
lester0866 <lester0866@gmail.com>
2022-04-19 18:26:43 -0400

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 msg=$1
-git add .
+git add *
 git commit -m "${1}"
 git push -u origin master
 
 hugo.exe # Generate the website inside the public directory
 cd public
-git add .
+git add *
 git commit -m "Build website"
 git push origin master
 cd ..
